7e15e8a694faf99a7f2c7ef30deda76b424e64ab
Charlotte Hearne <charlotte.hearne@asos.com>
2018-09-26 15:45:24 +0100

    Added stuff

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        private readonly Player _player1;
+        private readonly Player _player2;
+        private readonly Guid _gameId;
+        private readonly GameBoard _board;
+
+        public Game(Player player1, Player player2, Guid gameId)
+        {
+            _player1 = player1;
+            _player2 = player2;
+            _gameId = gameId;
+            _board = new GameBoard();
+
+        }
+
+    }
+
+}
---
+++ TicTacToe/GameBoard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameBoard
+    {
+        private const int Size = 9;
+        private readonly GameSign[] _board;
+
+        public GameBoard()
+        {
+            _board = new GameSign[Size];
+            
+        }
+
+        public bool IsValidMove(int position, GameSign sign) =>
+            _board[position] == GameSign.Empty;
+
+        public void MakeMove(int position, GameSign sign)
+        {
+            if (!IsValidMove(position, sign))
+                return;
+            _board[position] = sign;
+        }
+
+    }
+}
